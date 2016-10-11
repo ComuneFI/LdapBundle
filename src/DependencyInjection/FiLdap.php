ee10f718d585c628b10c169ed808af0a74e0fc42
d59495 <andrea.manzi@comune.fi.it>
2016-10-11 10:19:25 +0200

    refactoring

--- src/DependencyInjection/FiLdap.php
+++ src/DependencyInjection/FiLdap.php
@@ -2,8 +2,8 @@
 
 namespace Fi\LdapBundle\DependencyInjection;
 
-class FiLdap {
-
+class FiLdap
+{
     private $ldapHost;
     private $ldapPort;
     private $ldapUser;
@@ -14,71 +14,34 @@ class FiLdap {
     private $bind;
     private $connection;
 
-    public function __construct($parms) {
-        $this->ldapHost = $this->getLdapHostParam($parms);
-        $this->ldapPort = $this->getLdapPortParam($parms);
-        $this->ldapUser = $this->getLdapUsernameParam($parms);
-        $this->ldapPwd = $this->getLdapPasswordParam($parms);
-        $this->basedn = $this->getLdapBasednParam($parms);
-        $this->filterutenti = $this->getLdapUserFilterParam($parms);
-        $this->attributi = $this->getLdapAttributeParam($parms);
+    public function __construct($parms)
+    {
+        $this->ldapHost = FiLdapUtils::getLdapHostParam($parms);
+        $this->ldapPort = FiLdapUtils::getLdapPortParam($parms);
+        $this->ldapUser = FiLdapUtils::getLdapUsernameParam($parms);
+        $this->ldapPwd = FiLdapUtils::getLdapPasswordParam($parms);
+        $this->basedn = FiLdapUtils::getLdapBasednParam($parms);
+        $this->filterutenti = FiLdapUtils::getLdapUserFilterParam($parms);
+        $this->attributi = FiLdapUtils::getLdapAttributeParam($parms);
 
         $this->connect();
     }
 
-    private function getLdapHostParam($parms) {
-        return (isset($parms['host'])) ? $parms['host'] : '';
+    private function getLdapUsernameUtenti($parms)
+    {
+        return FiLdapUtils::getLdapUsernameParam($parms);
     }
 
-    private function getLdapPortParam($parms) {
-        return (isset($parms['port'])) ? $parms['port'] : '';
-    }
-
-    private function getLdapUsernameParam($parms) {
-        return (isset($parms['username'])) ? $parms['username'] : '';
-    }
-
-    private function getLdapPasswordParam($parms) {
-        return (isset($parms['password'])) ? $parms['password'] : '';
-    }
-
-    private function getLdapBasednParam($parms) {
-        return (isset($parms['basedn'])) ? $parms['basedn'] : '';
-    }
-
-    private function getLdapUserFilterParam($parms) {
-        return (isset($parms['userfilter'])) ? $parms['userfilter'] : '';
-    }
-
-    private function getLdapAttributeParam($parms) {
-        return (isset($parms['attribute'])) ? $parms['attribute'] : array();
-    }
-
-    private function getLdapBasednUtenti($parms) {
-        return (isset($parms['base_dn'])) ? $parms['base_dn'] : $this->basedn;
-    }
-
-    private function getLdapUserFilterUtenti($parms) {
-        return (isset($parms['filter'])) ? $parms['filter'] : $this->filterutenti;
-    }
-
-    private function getLdapAttributeUtenti($parms) {
-        return (isset($parms['attribute']) && is_array($parms['attribute'])) ? $parms['attribute'] : $this->attributi;
-    }
-
-    private function getLdapUsernameUtenti($parms) {
-        return $this->getLdapUsernameParam($parms);
-    }
-
-    public function getUtenti($parms = array()) {
-        $base_dn = $this->getLdapBasednUtenti($parms);
-        $filter = $this->getLdapUserFilterUtenti($parms);
-        $attributi = $this->getLdapAttributeUtenti($parms);
+    public function getUtenti($parms = array())
+    {
+        $base_dn = FiLdapUtils::getLdapBasednUtenti($parms);
+        $filter = FiLdapUtils::getLdapUserFilterUtenti($parms);
+        $attributi = FiLdapUtils::getLdapAttributeUtenti($parms);
 
         $read = ldap_search($this->connection, $base_dn, $filter, $attributi);
 
         if (!$read) {
-            throw new \Exception("Unable to search ldap server");
+            throw new \Exception('Unable to search ldap server');
         }
 
         $users = array();
@@ -104,20 +67,21 @@ class FiLdap {
         return $users;
     }
 
-    public function getUserInformation($parms = array()) {
-        $base_dn = $this->getLdapBasednUtenti($parms);
-        $username = $this->getLdapUsernameUtenti($parms);
+    public function getUserInformation($parms = array())
+    {
+        $base_dn = FiLdapUtils::getLdapBasednUtenti($parms);
+        $username = FiLdapUtils::getLdapUsernameUtenti($parms);
 
         if (strlen($username) > 0) {
             $filter = "(&(objectClass=user)(cn=$username))";
         } else {
-            $filter = $this->getLdapUserFilterUtenti($parms);
+            $filter = FiLdapUtils::getLdapUserFilterUtenti($parms);
         }
 
         $read = ldap_search($this->connection, $base_dn, $filter);
 
         if (!$read) {
-            throw new Exception("Unable to search ldap server");
+            throw new Exception('Unable to search ldap server');
         }
 
         $info = ldap_get_entries($this->connection, $read);
@@ -135,34 +99,38 @@ class FiLdap {
         return $userinfo;
     }
 
-    public function connect() {
+    public function connect()
+    {
         try {
             $this->connection = ldap_connect($this->ldapHost, $this->ldapPort);
             ldap_set_option($this->connection, LDAP_OPT_PROTOCOL_VERSION, 3);
             ldap_set_option($this->connection, LDAP_OPT_REFERRALS, 0);
             $this->bind = ldap_bind($this->connection, $this->ldapUser, $this->ldapPwd);
         } catch (Exception $exc) {
-            return array('errcode' => -1, 'message' => 'Impossibile connettersi al server LDAP: ' . $exc->getTraceAsString());
+            return array('errcode' => -1, 'message' => 'Impossibile connettersi al server LDAP: '.$exc->getTraceAsString());
         }
 
         return array('errcode' => 0, 'message' => 'Connesso al server LDAP');
     }
 
-    public function disconnect() {
+    public function disconnect()
+    {
         try {
             ldap_close($this->connection);
         } catch (Exception $exc) {
-            return array('errcode' => -1, 'message' => 'Impossibile disconnettersi dal server LDAP: ' . $exc->getTraceAsString());
+            return array('errcode' => -1, 'message' => 'Impossibile disconnettersi dal server LDAP: '.$exc->getTraceAsString());
         }
 
         return array('errcode' => 0, 'message' => 'Disonnesso dal server LDAP');
     }
 
-    private function getValue($info, $ldapattribute, $index) {
+    private function getValue($info, $ldapattribute, $index)
+    {
         return isset($info[$index][$ldapattribute][0]) ? $info[$index][$ldapattribute][0] : '';
     }
 
-    public function cercaUtente($arrayUtenti, $search_text) {
+    public function cercaUtente($arrayUtenti, $search_text)
+    {
         //Funzione utile per cercare utenti estraendo una sola volta da ldap
         //e cercando nell'array invece che via ldap
         set_time_limit(0);
@@ -188,35 +156,36 @@ class FiLdap {
         return $matches;
     }
 
-    public function dumpUtenti($parms = array()) {
-        $base_dn = $this->getLdapBasednUtenti($parms);
-        $filter = $this->getLdapUserFilterUtenti($parms);
-        $attributi = $this->getLdapAttributeUtenti($parms);
+    public function dumpUtenti($parms = array())
+    {
+        $base_dn = FiLdapUtils::getLdapBasednUtenti($parms);
+        $filter = FiLdapUtils::getLdapUserFilterUtenti($parms);
+        $attributi = FiLdapUtils::getLdapAttributeUtenti($parms);
 
         if (count($attributi) === 0) {
             $read = ldap_search($this->connection, $base_dn, $filter);
 
             if (!$read) {
-                throw new \Exception("Unable to search ldap server");
+                throw new \Exception('Unable to search ldap server');
             }
         } else {
             $read = ldap_search($this->connection, $base_dn, $filter, $attributi);
 
             if (!$read) {
-                throw new \Exception("Unable to search ldap server");
+                throw new \Exception('Unable to search ldap server');
             }
         }
 
         $info = ldap_get_entries($this->connection, $read);
 
         //Per visualizzare tutti i dettagli in Active Directory
-        echo $info['count'] . ' entrees retournees<br/><br/>';
+        echo $info['count'].' entrees retournees<br/><br/>';
         for ($ligne = 0; $ligne < $info['count']; ++$ligne) {
             for ($colonne = 0; $colonne < $info[$ligne]['count']; ++$colonne) {
                 $data = $info[$ligne][$colonne];
                 switch ($data) {
                     case 'accountexpires':
-                        echo $data . ':' . $this->getFormatTime($info[$ligne][$data][0]) . '<br/>';
+                        echo $data.':'.FiLdapUtils::getFormatTime($info[$ligne][$data][0]).'<br/>';
                         break;
 
                     case 'thumbnailphoto':
@@ -225,22 +194,15 @@ class FiLdap {
                         file_put_contents($tempFile, $imageString);
                         $finfo = new \finfo(FILEINFO_MIME_TYPE);
                         $mime = explode(';', $finfo->file($tempFile));
-                        echo '<img src="data:' . $mime[0] . ';base64,' . base64_encode($imageString) . '"/>';
+                        echo '<img src="data:'.$mime[0].';base64,'.base64_encode($imageString).'"/>';
                         break;
 
                     default:
-                        echo $data . ':' . $info[$ligne][$data][0] . '<br/>';
+                        echo $data.':'.$info[$ligne][$data][0].'<br/>';
                         break;
                 }
             }
             echo '<br/>';
         }
     }
-
-    private function getFormatTime($time) {
-        $winSecs = (int) ($time / 10000000); // divide by 10 000 000 to get seconds
-        $unixTimestamp = ($winSecs - 11644473600); // 1.1.1600 -> 1.1.1970 difference in seconds
-        return date(\DateTime::RFC822, $unixTimestamp);
-    }
-
 }
---
+++ src/DependencyInjection/FiLdapUtils.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Fi\LdapBundle\DependencyInjection;
+
+class FiLdapUtils
+{
+    public static function getFormatTime($time)
+    {
+        $winSecs = (int) ($time / 10000000); // divide by 10 000 000 to get seconds
+        $unixTimestamp = ($winSecs - 11644473600); // 1.1.1600 -> 1.1.1970 difference in seconds
+        return date(\DateTime::RFC822, $unixTimestamp);
+    }
+
+    public static function getLdapHostParam($parms)
+    {
+        return (isset($parms['host'])) ? $parms['host'] : '';
+    }
+
+    public static function getLdapPortParam($parms)
+    {
+        return (isset($parms['port'])) ? $parms['port'] : '';
+    }
+
+    public static function getLdapUsernameParam($parms)
+    {
+        return (isset($parms['username'])) ? $parms['username'] : '';
+    }
+
+    public static function getLdapPasswordParam($parms)
+    {
+        return (isset($parms['password'])) ? $parms['password'] : '';
+    }
+
+    public static function getLdapBasednParam($parms)
+    {
+        return (isset($parms['basedn'])) ? $parms['basedn'] : '';
+    }
+
+    public static function getLdapUserFilterParam($parms)
+    {
+        return (isset($parms['userfilter'])) ? $parms['userfilter'] : '';
+    }
+
+    public static function getLdapAttributeParam($parms)
+    {
+        return (isset($parms['attribute'])) ? $parms['attribute'] : array();
+    }
+
+    public static function getLdapBasednUtenti($parms)
+    {
+        return (isset($parms['base_dn'])) ? $parms['base_dn'] : $this->basedn;
+    }
+
+    public static function getLdapUserFilterUtenti($parms)
+    {
+        return (isset($parms['filter'])) ? $parms['filter'] : $this->filterutenti;
+    }
+
+    public static function getLdapAttributeUtenti($parms)
+    {
+        return (isset($parms['attribute']) && is_array($parms['attribute'])) ? $parms['attribute'] : $this->attributi;
+    }
+}
